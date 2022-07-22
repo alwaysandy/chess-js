08a7cc6f4a87d16afe8c574f1eb2775893dbe433
Andrew Daurio-Sas <andrewdaurio@gmail.com>
2022-07-22 18:44:26 -0400

    First commit, barebones site made with ability to add chessboard

---
+++ script.js
@@ -0,0 +1,35 @@
+function createBoard() {
+    let board = [];
+    const boardDiv = document.querySelector('div.board');
+    console.log(boardDiv);
+    for (let y = 0; y < 8; y++) {
+        board.push([]);
+        const line = document.createElement('div');
+        line.classList.add('line');
+        for (let x = 0; x < 8; x++) {
+            let tile = document.createElement('div');
+            tile.classList.add('tile');
+            tile.dataset.x = x;
+            tile.dataset.y = y;
+
+            if (y % 2 == 0) {
+                if (x % 2 == 0) {
+                    tile.classList.add('white-tile');
+                } else {
+                    tile.classList.add('black-tile');
+                }
+            } else {
+                if (x % 2 == 0) {
+                    tile.classList.add('black-tile');
+                } else {
+                    tile.classList.add('white-tile');
+                }
+            }
+            line.appendChild(tile);
+            board[y].push(tile)
+        }
+        boardDiv.appendChild(line);
+    }
+}
+
+createBoard();
