2ac931f349ceff7132ac7a9d51c85c0ff655f34c
Andrew Daurio-Sas <andrewdaurio@gmail.com>
2022-07-22 22:48:51 -0400

    Add ability to place pieces on board

--- script.js
+++ script.js
@@ -34,11 +34,108 @@ function createBoard() {
     return board;
 }
 
+function createPieceData(piece, colour, directions) {
+    return {
+        piece:piece,
+        colour:colour,
+        directions:directions,
+    }
+}
+
+function createPieceImageElements(piece) {
+    let whitePiece = document.createElement('img');
+    let blackPiece = document.createElement('img');
+    blackPiece.setAttribute("src", `./chessvgs/${piece}-black.svg`);
+    whitePiece.setAttribute("src", `./chessvgs/${piece}-white.svg`);
+    
+    blackPiece.classList.add('piece');
+    whitePiece.classList.add('piece');
+    return {black: blackPiece, white: whitePiece};
+}
+
+function createAndPlace(piece, offset) {
+    for (let i = 0; i < 2; i++) {
+        const pieces = createPieceImageElements(piece);
+        if (i == 0) {
+            board[0][offset].appendChild(pieces[1]);
+            board[7][offset].appendChild(pieces[0]);
+        } else {
+            board[0][7 - offset].appendChild(pieces[1]);
+            board[7][7 - offset].appendChild(pieces[0]);
+        }
+    }
+}
+
 function placePieces() {
-    const bqueen = document.createElement('img');
-    bqueen.setAttribute("src", "./chessvgs/queen-black.svg")
-    bqueen.classList.add('piece');
-    board[0][0].appendChild(bqueen);
+    let pieces = []
+    let pieceNodes;
+    for (let x = 0; x < 8; x++) { 
+        if (x == 0 || x == 7) {
+            nodes = createPieceImageElements("rook");
+            nodes.black.dataset.x = x;
+            nodes.black.dataset.y = 0;
+            nodes.white.dataset.x = x;
+            nodes.white.dataset.y = 7;
+            board[0][x].appendChild(nodes.black);
+            board[7][x].appendChild(nodes.white);
+        }
+
+        if (x == 1 || x == 6) {
+            nodes = createPieceImageElements("knight");
+            nodes.black.dataset.x = x;
+            nodes.black.dataset.y = 0;
+            nodes.white.dataset.x = x;
+            nodes.white.dataset.y = 7;
+            board[0][x].appendChild(nodes.black);
+            board[7][x].appendChild(nodes.white);
+        }
+
+        if (x == 2 || x == 5) {
+            nodes = createPieceImageElements("bishop");
+            nodes.black.dataset.x = x;
+            nodes.black.dataset.y = 0;
+            nodes.white.dataset.x = x;
+            nodes.white.dataset.y = 7;
+            board[0][x].appendChild(nodes.black);
+            board[7][x].appendChild(nodes.white);
+        }
+
+        if (x == 3) {
+            nodes = createPieceImageElements("queen");
+            nodes.black.dataset.x = x;
+            nodes.black.dataset.y = 0;
+            nodes.white.dataset.x = x;
+            nodes.white.dataset.y = 7;
+            board[0][x].appendChild(nodes.black);
+            board[7][x].appendChild(nodes.white);
+        }
+
+        if (x == 4) {
+            nodes = createPieceImageElements("king");
+            nodes.black.dataset.x = x;
+            nodes.black.dataset.y = 0;
+            nodes.white.dataset.x = x;
+            nodes.white.dataset.y = 7;
+            board[0][x].appendChild(nodes.black);
+            board[7][x].appendChild(nodes.white);
+        }
+
+        nodes = createPieceImageElements("pawn");
+        nodes.black.dataset.x = x;
+        nodes.black.dataset.y = 1;
+        nodes.white.dataset.x = x;
+        nodes.white.dataset.y = 6;
+        board[1][x].appendChild(nodes.black);
+        board[6][x].appendChild(nodes.white);
+    }
+
+
+    for (let y = 0; y < board.length; y++) {
+        pieces.push([]);
+        for (let x = 0; x < board[y].length; x++) {
+           pieces[y].push(0); 
+        }
+    }
 }
 
 let board = createBoard();
